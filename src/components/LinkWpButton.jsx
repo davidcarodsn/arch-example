fffe1496a15d7d73b46e9a66b7f90f43f0cb8669
David Caro <david@z2h.it>
2023-04-14 16:43:06 -0300

    Wpp buttons redesign

--- src/components/LinkWpButton.jsx
+++ src/components/LinkWpButton.jsx
@@ -1,45 +1,23 @@
 import React from 'react'
 
 
-const LinkWpButton = ({ data }) => {
+const LinkWpButton = ({ data, index }) => {
   return (
     <a 
       style={{ 
-        color: '#444444', 
-        backgroundColor: '#ebcd1e', 
-        height: '50px', 
+        color: 'white', 
+        backgroundColor: (index % 2) ? '#e91d25' : '#0a75ba', 
+        height: '48px', 
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'center',
         borderRadius: '150px 150px 150px 150px',
         width: '90%'
       }} 
       target='_blank' 
       href={`https://api.whatsapp.com/send?phone=${data.phone}&text=`}
     >
-      <div style={{ width: '90%', display: 'flex' }}>
-        <img 
-          style={{ 
-            width: '60px', 
-            borderRadius: '100%', 
-            marginRight: '30px', 
-            transform: 'translateX(-2px)' 
-          }} 
-          src={data.img} 
-          alt={`${data.localization}`} 
-        />
-        <div 
-          style={{ 
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            paddingTop: '7px'
-          }}
-        >
-          <h5 fontSize='12px'>{data.area}</h5>
-          <span style={{ fontSize: '12px' }}>{data.localization}</span>
-        </div>
-      </div>
-      <i style={{ fontSize: '25px' }} class="zmdi zmdi-chevron-right"></i>
+      <h5 style={{ color: 'white' }}>{data.area}</h5>        
     </a>
   )
 }
--- src/components/WhatsAppButtonComponent.jsx
+++ src/components/WhatsAppButtonComponent.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
 import WppIcon from '../assets/img/wpp-icon.png';
+import WppIconHead from '../assets/img/ws-head.png';
 import LinkWpButton from './LinkWpButton';
-import ConcordiaImg from '../assets/img/concordia.png'
 
 const WhatsAppButtonComponent = () => {
-  const [isLinksDisplayActive, setIsLinksDisplayActive] = useState(false);
+  const [isLinksDisplayActive, setIsLinksDisplayActive] = useState(true);
 
   const phonesData = [
     {
       phone: '543446374867',
-      area: 'Ventas',
-      localization: 'Concordia',
-      img: ConcordiaImg
+      area: 'Ventas Concordia',
     },
     {
-      phone: '',
-      area: '',
-      localization: '',
-      img: ''
+      phone: '543446507870',
+      area: 'Administracion Corcordia',
+    },
+    {
+      phone: '543446604155',
+      area: 'Ventas Colon',
+    },
+    {
+      phone: '543446583228',
+      area: 'Ventas Villa Elisa',
+    },
+    {
+      phone: '543446354009',
+      area: 'Ventas/administracion CdelU',
+    },
+    {
+      phone: '543446636419',
+      area: 'Ventas Gualeguaychu',
+    },
+    {
+      phone: '543446385621',
+      area: 'Administracion Gualeguaychu',
+    },
+    {
+      phone: '543446347821',
+      area: 'Ventas Urdinarrain',
+    },
+    {
+      phone: '54344659841',
+      area: 'Administracion Urdinarrain',
     },
   ]
   return (
@@ -33,26 +57,32 @@ const WhatsAppButtonComponent = () => {
               }}
             >
               <div style={{
-                  height: '60px',
-                  backgroundColor: '#ebcd1e',
+                  height: '100px',
+                  backgroundColor: '#adafb3',
                   borderRadius: '10px 10px 0px 0px',
                   display: 'flex',
                   alignItems: 'center',
-                  justifyContent: 'space-between',
+                  justifyContent: 'space-evenly',
                   padding: '20px'
                 }}
               >
-                <span style={{ fontWeight: '700' }}>Comunícate con nosotros.</span>
+                <img src={WppIconHead} width="70px" alt="wpp-logo" />
+                <span style={{ color: '#126fb2', fontWeight: '700', fontSize: '20px' }}>
+                  ¡Comunícate 
+                  <br />
+                  con nosotros!
+                </span>
                 <button 
                   onClick={() => setIsLinksDisplayActive(false)}
                   style={{
                     fontSize: '15px',
                     cursor: 'pointer',
                     width: '7%',
-                    color: '#ebcd1e',
-                    backgroundColor: '#87760f',
+                    color: '#adafb3',
+                    backgroundColor: '#54575c',
                     border: 'none',
-                    borderRadius: '100%'
+                    borderRadius: '100%',
+                    marginLeft: '55px'
                   }}
                 > 
                   X
@@ -61,21 +91,19 @@ const WhatsAppButtonComponent = () => {
               <div 
                 style={{
                   height: '340px',
-                  padding: '10px'
+                  padding: '10px',
+                  overflow: 'auto'
                 }}
               >
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
                     gap: '10px',
-                    height: '100%',
-                    overflowY: 'scroll',
                     alignItems: 'center'
-                    
                   }}
                 >
                   {
-                    phonesData.map(data => <LinkWpButton data={data} />)
+                    phonesData.map((data, i )=> <LinkWpButton data={data} index={i} />)
                   }
                 </div>  
               </div>
